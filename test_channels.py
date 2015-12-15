f6959e4e984ef145d23c6916fe256867ae4736e9
suroor <surusuroor@gmail.com>
2015-12-15 15:39:07 +0530

    tewsts

--- test_channels.py
+++ test_channels.py
@@ -1,2 +1,12 @@
 import channels
 from mock import Mock
+import facebook
+
+def test_facebook_broadcast():
+    mock = Mock(facebook.GraphAPI)
+    face_book = channels.fb("token", "message")
+    face_book.broadcast(mock)
+
+    assert mock.put_object.called
+    assert mock.put_object.called_with("me", "feed", message = "message")
+
